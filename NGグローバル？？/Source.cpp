31859acadfa1790a1457cac7a6b90dfd4c3a54e8
Satori32 <Huga8989+githubf@gmail.com>
2019-10-14 18:02:12 +0900

    プロジェクト ファイルを追加します。

---
+++ NGグローバル？？/Source.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+template<class T,class... A>
+T& Global(const A&... Arg) {
+	static T V(Arg...);
+	return V;
+}
+
+int main() {
+
+	Global<int,int>(25);
+	int N = Global<int,int>(0);//return 25;
+
+	return 0;
+
+}
